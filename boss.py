cb8b981c8d9d09af995ce311c7a69905dede94ed
Mohamed Baltamr <mohamed.m.baltamr@gmail.com>
2026-04-11 22:32:29 +0200

    Add files via upload

--- boss.py
+++ boss.py
@@ -5,48 +5,46 @@ from boss_class import *
 students = []
 Average_grades_for_class = []
 
+
 while True:
     option = get_option(["Add Student", "Display class statistics", "Save to file and exit"])
 
     if option == 0:
-
-
-        name = str(input("Adding name of student :\n"))
-
+        name = input("Adding name of student :\n")
         a, b, c = get_grade(), get_grade(), get_grade()
-        abc = a, b, c
+        abc = [a, b, c]
 
         Average_grades = st.mean(abc)
-        result = get_result(Average_grades)
 
+        result = get_result(Average_grades)
         student = get_canvas(a, b, c, name, Average_grades, result)
 
         students.append(student)
         Average_grades_for_class.append(Average_grades)
 
-    elif option == 1:
 
-        if not Average_grades_for_class:
-            print("There is not enough data! Add a student first.")
+    elif option == 1:
+        if not students:
+            no_data_message()
             continue 
-            
-    
-        print(get_canvas_file(students))
-        rate_class = st.mean(Average_grades_for_class)
-        result = get_result(rate_class)
-        print("The class success rate is: %.1f %% " % (rate_class), "The Result is : %s"%(result))
+
+        student_data = str(get_canvas_file(students))+"\n\n\n"
+        class_data = str(get_canvas_file_02(Average_grades_for_class))
+
+        print(student_data)
+        print(class_data)
 
 
 
     elif option == 2:
-        if not Average_grades_for_class:
-            print("There is not enough data! Add a student first.")
+        if not students:
+            no_data_message()
             continue 
-        aaa = students 
-        bbb = Average_grades_for_class 
-        x = open("students.txt", "w")
-        x.write(str(get_canvas_file(aaa))+"\n\n\n")
-        x.write(str(get_canvas_file_02(bbb)))
-        x.close()
+
+        student_data = str(get_canvas_file(students))+"\n\n\n"
+        class_data = str(get_canvas_file_02(Average_grades_for_class))  
+
+        with open("students_data.txt", "w") as x:
+            x.write(student_data)
+            x.write(class_data)
         exit()
- 
--- boss_class.py
+++ boss_class.py
@@ -1,14 +1,13 @@
 import random as ra
 import statistics as st
 
-
 def get_number_only():
-        while True:
-            try:
-                number = int(input("Enter a number:\n"))
-                return number
-            except ValueError:
-                print("Invalid input. Please enter a valid number.")
+    while True:
+        try:
+            number = int(input("Enter a number:\n"))
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 def get_option(op):
     print("menu:\n")
     for i in range(len(op)):
@@ -33,11 +32,19 @@ def get_result(average_for_f):
         result = "Excellent"
     return result
 def get_canvas(a101, b101, c101, name101, Average_grades101, result101):
-    edit = ((name101.ljust(20)).lower()).title(), {"math": a101}, {"english": b101}, {"programming": c101}
-    student = [(str(edit)).ljust(85),"Score Rate (%.1f %%)"%(Average_grades101) ," Result : %s"%(result101)]
+
+    mep1 = ({"math": a101}, {"english": b101}, {"programming": c101})
+    mep = str(mep1).ljust(60)
+
+    edit = ((name101.ljust(45)).lower()).title() + mep
+    student = [(str(edit)).ljust(85) + "Score Rate (%.1f %%) , Result : %s"%(Average_grades101, result101)]
     return student
 def get_canvas_file(students):
     return "\n".join(str(s) for s in students)
 def get_canvas_file_02(Average_grades_for_class):
     mean_class = st.mean(Average_grades_for_class)
-    return "The class success rate is: %.1f %% " % (mean_class), "The Result is : %s"%(get_result(mean_class))
+    xx = "The class success rate is: %.1f %% , The Result is : %s"%(mean_class, get_result(mean_class))
+    xy = xx.center(144)
+    return xy
+def no_data_message():
+    print("There is not enough data! Add a student first.")
